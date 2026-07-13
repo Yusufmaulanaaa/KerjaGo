14250739240fb57f77235e9caa810073958e7aa8
Yusuf Maulana <yusufmaulanasyifa25@gmail.com>
2026-07-13 11:53:33 +0700

    fix: switch to HashRouter for guaranteed SPA routing on Vercel

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { JobProvider } from './context/JobContext'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <JobProvider>
         <App />
       </JobProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
